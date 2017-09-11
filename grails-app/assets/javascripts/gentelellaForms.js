941f23f5e30b75b4f8c3bd4692ad264bb46c44c2
Zoran Bubric <zbubric@gmail.com>
2017-09-11 18:37:39 +0200

    Assets naming aligned

--- grails-app/assets/javascripts/gentelellaForms.js
+++ grails-app/assets/javascripts/gentelellaForms.js
@@ -1,17 +1,10 @@
-// This is a manifest file that'll be compiled into application.js.
+// This is set of JS needed for advanced form controls
 
-//= require jquery/dist/jquery.min.js
-//= require bootstrap/dist/js/bootstrap.min.js
-//= require fastclick/lib/fastclick.js
-//= require nprogress/nprogress.js
-//= require pnotify/dist/pnotify.js
-//= require pnotify/dist/pnotify.buttons.js
-//= require pnotify/dist/pnotify.nonblock.js
+
 //= require iCheck/icheck.js
 //= require moment/moment.js
 //= require bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js
 //= require jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js
-//= require gentelellaOrigin.js
 
 /**
  * Load dateTimePicker for date fields
