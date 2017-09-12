431df8238fdaabf63359d851539cc9bb12306046
Zoran Bubric <zbubric@gmail.com>
2017-09-12 18:54:58 +0200

    Added select2 library

--- grails-app/assets/javascripts/gentelella.js
+++ grails-app/assets/javascripts/gentelella.js
@@ -11,15 +11,20 @@
 //= require moment/moment.js
 //= require bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js
 //= require jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js
+//= require select2/dist/js/select2.min.js
 //= require gentelellaOrigin.js
 
 /**
  * Load dateTimePicker for date fields
  */
 $(document).ready(function() {
+
+    // Load dateTimePicker for date fields
     $('.datepicker').datetimepicker({
-        //format: 'L',              
-        format: 'DD/MM/YYYY'
+        //format: 'L'
     });
+
+    // Load input masks for all fields
+    $(".masked").inputmask();
 
 });
--- grails-app/assets/javascripts/gentelellaForms.js
+++ grails-app/assets/javascripts/gentelellaForms.js
@@ -11,7 +11,6 @@
  */
 $(document).ready(function() {
     $('.datepicker').datetimepicker({
-        //format: 'L',              
         format: 'DD/MM/YYYY'
     });
 
