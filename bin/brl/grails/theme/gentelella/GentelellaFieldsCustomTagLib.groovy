a73fad02a118a9809ae9b03f191cd79463c4f885
ezorbub <zoran.bubric@.ericsson.com>
2017-11-03 14:09:21 +0100

    Gent fields improvements

--- bin/brl/grails/theme/gentelella/GentelellaFieldsCustomTagLib.groovy
+++ bin/brl/grails/theme/gentelella/GentelellaFieldsCustomTagLib.groovy
@@ -20,55 +20,71 @@ class GentelellaFieldsCustomTagLib{
     
     /**
     * Gentelella owerride of f:field tag
-    * Used to inject specific paramts to widget (css classes and masks)
+    * Injects spcific css classes to widget (form-control or flat, depending on type)
+    * Also, handles Gentelella specific attributes:
+    *   mask: set input mask for widget
+    *   iconleft: left 'feedback icon' (should be also handled in wrapper)
+    *   iconRight: right 'feedback icon' (should be also handled in wrapper)
+    *   plain: if attribute is set (any value) standard Field widget will be rendered (no gentelella customisation)
     *
-    * TODO: Add config option to skip custom part (default behaviur)
     **/
     def field = { attrs, body ->
-            
-        // list of types that can apply mask
-        final maskedTypes = [String, Number, URL, Date, Calendar, java.sql.Date, java.sql.Time]
 
-        // get bean name
-        def bean
-        if( attrs.bean ){
-            // get bean name from page scope variables (case for f:field)
-            bean = pageScope.variables[attrs.bean]
-        } else {
-            // get bean name from bean stack (case for f:all and f:with)
-            bean = formFieldsTagLib.getBeanStack().bean
-        }
+        // skip customisation if plain='yes'
+        if( !attrs.plain ){
+    
+            // list of types that can apply mask
+            final maskedTypes = [String, Number, URL, Date, Calendar, java.sql.Date, java.sql.Time]
 
-        // resolve property
-        BeanPropertyAccessor propertyAccessor = beanPropertyAccessorFactory.accessorFor(bean, attrs.property)     
-
-        if( propertyAccessor ){
-            // field type
-            def type = propertyAccessor.propertyType
-            // widget class (as defined in gsp tag)
-            def widgetClass = attrs.'widget-class' ?: ''
-
-            // set base css class depending on widget type
-            if( type in [boolean, Boolean] ){                
-                widgetClass += "flat"
+            // get bean name
+            def bean
+            if( attrs.bean ){
+                // get bean name from page scope variables (case for f:field)
+                bean = pageScope.variables[attrs.bean]
             } else {
-                widgetClass += "form-control"
+                // get bean name from bean stack (case for f:all and f:with)
+                bean = formFieldsTagLib.getBeanStack().bean
             }
 
-            // set mask properties for applicable widgets
-            if( attrs.mask && (type in MASKED_TYPES) ){
-                // masks are not applicable for associations
-                if( !propertyAccessor.persistentProperty.properties.containsKey('associatedEntity')){
-                    // apend css class for masked components
-                    widgetClass += " masked"  
-                    // add widget mask parameter
-                    attrs.'widget-data-inputmask' = "'mask' : '$attrs.mask'"
+            // resolve property
+            BeanPropertyAccessor propertyAccessor = beanPropertyAccessorFactory.accessorFor(bean, attrs.property)     
+
+            if( propertyAccessor ){
+                // field type
+                def type = propertyAccessor.propertyType
+                // widget class (as defined in gsp tag)
+                def widgetClass = attrs.'widget-class' ?: ''
+
+                // set base css class depending on widget type
+                if( type in [boolean, Boolean] ){                
+                    widgetClass += "flat"
+                } else {
+                    widgetClass += "form-control"
                 }
+
+                // set mask properties for applicable widgets
+                if( attrs.mask && (type in MASKED_TYPES) ){
+                    // masks are not applicable for associations
+                    if( !propertyAccessor.persistentProperty.properties.containsKey('associatedEntity')){
+                        // apend css class for masked components
+                        widgetClass += " masked"  
+                        // add widget mask parameter
+                        attrs.'widget-data-inputmask' = "'mask' : '$attrs.mask'"
+                    }
+                }
+
+                // add new widget class param
+                attrs.'widget-class' = widgetClass            
             }
 
-            // add new widget class param
-            attrs.'widget-class' = widgetClass            
-        }
+            // add css class for feedback icons
+            if( attrs.iconLeft ) {
+                attrs.'widget-class' += " has-feedback-left"
+            }
+            if( attrs.iconRight ) {
+                attrs.'widget-class' += " has-feedback-right"
+            } 
+        }       
 
         // formFieldsTagLib.widget.call(attrs)
         formFieldsTagLib.field.call(attrs, body)   
