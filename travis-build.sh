0fdc797d3e76ee38060f183cc6f171122f674584
zbubric <zbubric@gmail.com>
2017-12-06 09:06:53 -0500

    GITHUB_TOKEN defined

--- travis-build.sh
+++ travis-build.sh
@@ -17,9 +17,9 @@ if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST
   git config --global user.name "$GIT_NAME"
   git config --global user.email "$GIT_EMAIL"
   git config --global credential.helper "store --file=~/.git-credentials"
-  echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+  echo "https://$GITHUB_TOKEN:@github.com" > ~/.git-credentials
 
-  git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
+  git clone https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
   cd gh-pages
 
   # If this is the master branch then update the snapshot
