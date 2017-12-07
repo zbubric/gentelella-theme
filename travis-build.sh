61024b6267ae5185c6df2566e8336ddd6ac8af14
zbubric <zbubric@gmail.com>
2017-12-07 09:44:00 -0500

    Ready for first deployment

--- travis-build.sh
+++ travis-build.sh
@@ -11,6 +11,12 @@ echo "Publishing archives for branch $TRAVIS_BRANCH"
 if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
   echo "Publishing archives"
+  
+  if [[ -n $TRAVIS_TAG ]]; then
+      ./gradlew bintrayUpload || EXIT_STATUS=$?
+  else
+      ./gradlew publish || EXIT_STATUS=$?
+  fi
 
   ./gradlew docs || EXIT_STATUS=$?
 
